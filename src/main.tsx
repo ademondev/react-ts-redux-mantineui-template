b350842fea69a6fbfb684f5040b66037a09b07b6
Raki <killer.vp.mic@gmail.com>
2023-02-21 16:23:07 -0300

    readme.md | license

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import { MantineProvider } from '@mantine/core';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onOfflineReady() {
+    // Render a dialog or a modal to show the user offline mode is ready
+    alert('Offline is ready!');
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
